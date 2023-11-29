e430e6c1572bf88aae18f430841b73d2d1321ce1
Timur <example@gmail.com>
2023-11-29 13:55:16 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import cleanCSS from  'gulp-clean-css';
+import autoprefixer from 'gulp-autoprefixer';
+import rename from 'gulp-rename';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+
+gulp.task('server', function() {
+
+    browserSync({
+        server: {
+            baseDir: "src"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+})
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
